6a3f73d0d0ae0ca2260885d997fd3129da3367c7
davidylee123 <121922170+davidylee123@users.noreply.github.com>
2024-06-22 14:29:41 -0400

    Created Video Player Page

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
+
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDBkNjI5NWRmYTY1YTZjM2EzM2M3ZWNmMmU5MzQ5OCIsIm5iZiI6MTcxOTA3Nzg0OS4wMTcyNTYsInN1YiI6IjY2NzcwYWRhNTRkMGQwNzhmMWVkMmMzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zEiEZouVafet-ztoxk6xtZ_Ffgsi88DSs9B604Qi5KM",
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="titlecards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
+
+const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
+
+  return (
+    <div className="login">
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remeber Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have Account?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
