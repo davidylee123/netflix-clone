9daa06ae099a6b7a6d277320eb9848bf9ac84a74
davidylee123 <121922170+davidylee123@users.noreply.github.com>
2024-06-22 13:46:51 -0400

    Created Login Page

--- src/pages/Login/Login.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Login.css";
-
-const Login = () => {
-  return <div className="login"></div>;
-};
-
-export default Login;
--- src/pages/Player/Player.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Player.css";
-
-const Player = () => {
-  return <div className="player"></div>;
-};
-
-export default Player;
